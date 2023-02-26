b8d128b21dfc245e5a2b0395627b420039ef818e
Pushkaraj Lolusare <pushkarajlolusare24@gmail.com>
2023-02-26 19:49:26 +0530

    ok

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const connect = require("./config/mongoose.config")
 
 const app =  express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({origin:"*"}))
 app.use(express.urlencoded({extended:true}))
 app.use("/", laptop)
 
