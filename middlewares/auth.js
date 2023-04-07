e7db0af665f45a9bcce1f081ddcb878a2b5d6fb4
Pushkaraj Lolusare <pushkarajlolusare24@gmail.com>
2023-04-07 20:33:56 +0530

    back

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ var jwt = require("jsonwebtoken");
 const auth = async (req, res, next) => {
   const token = req.headers.authorization;
   if (token) {
-    const decoded = await jwt.verify(token, "karatlogin");
+    const decoded = await jwt.verify(token, process.env.key);
     if (decoded) {
       const userID = decoded.userID;
       req.userDetails = decoded;
